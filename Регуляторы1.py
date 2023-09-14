1126616215226f6fba23af66218e42103dff7bde
Alexander <sahaal2019@mail.ru>
2023-09-14 07:47:03 +0400

    Initial2

--- Регуляторы1.py
+++ Регуляторы1.py
@@ -8,14 +8,19 @@ with open("Desktop/Сценарии пробники/регуляторы_дан
   contacts_list = list(rows)
 pprint(contacts_list)
 
-# 1. Выполните пункты 1-3 задания.
-# Ваш 
- 
+
+pattern = re.compile(r'(\+7|8)\s*\(*(495)\)*\s*\-*(\d{3})[\-]*(\d{2})[\-]*(\d+)\s*(\(*\доб*\.\s*\d*\)*)?')  
+subst_pattern= r'+7(\2)\3-\4-\5 \6'
+result = pattern.sub(subst_pattern, contacts_list)
+print(result)
+
 
 # 2. Сохраните получившиеся данные в другой файл.
 # Код для записи файла в формате CSV:
-with open("phonebook.csv", "w") as f:
+with open("phonebook.csv", "w", encoding='utf=8') as f:
   datawriter = csv.writer(f, delimiter=',')
   
 ## Вместо contacts_list подставьте свой список:
-  datawriter.writerows(contacts_list) 
+  cont =[]
+  datawriter.writerows(cont) 
+pprint(cont)
